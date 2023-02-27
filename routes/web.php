363972bb3865b5bca57f2b8ee294bc63414c8a26
Alex-Prokhorenko <galexthunder@gmail.com>
2023-02-27 10:23:20 +0700

    add route, indexController and view for path '/'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Main\IndexController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespase' => 'Main'], function () {
+    Route::get('/', IndexController::class);
 });
+
 
 Auth::routes();
 
